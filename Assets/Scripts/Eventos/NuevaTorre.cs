5aefcb4986f780a5efdde21230a3475ac29a0352
Varguiniano <varguiniano@gmail.com>
2016-04-23 15:33:17 +0200

    Added two GPS events

---
+++ Assets/Scripts/Eventos/NuevaTorre.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace TodosLosEventos
+{
+	public class NuevaTorre : Evento
+	{
+		public NuevaTorre()
+		{
+			Nombre = "New GPS tower";
+			FechaInicio = GestorTiempo.fechaActual;
+		}
+		public override void Inicio()
+		{
+
+			/* TODO
+			 * Mostrar una interfaz con el siguiente texto:
+			 * You've found the old USSR's Mars 3 lander. You beieve you could use as a repeater for your GPS network.
+			 */
+
+			int chosed;
+
+			/* TODO
+			* Mostrar una lista con las opciones.
+			* 1. Invest some materials on getting the probe online.
+			* 2. Ignore it, those red machines are evil.
+			*/
+			if (chosed == 2) 
+			{
+				solucionado = false;
+				FechaFin = FechaInicio + new Fecha (10, 0, 0, 0);
+			} 
+			else 
+			{
+				solucionado = true;
+				FechaFin = FechaInicio + new Fecha (0, 30, 1, 0);
+			}
+		}
+
+		public override void Tick()
+		{
+		}
+		public override void Fin()
+		{
+			if (solucionado) {
+				/* TODO
+				* Mostrar en la interfaz el siguiente texto:
+				* You repaired the probe and it's now online on the coordinates (las que sean).
+				*/
+
+				/* TODO
+				* añadir una torre sobre esas coordenadas (probablemente elegidas de manera random entre partes del mapa no cubiertas.
+				* reducir X cantidad de materiales.
+				*/
+			} 
+			else 
+			{
+				/* TODO
+				 * Mostrar en la interfaz el siguiente texto:
+				 * You decide to leave the good old Mars 3 alone, you'll build your own network.
+				 */ 
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Eventos/NuevoSatelite.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TodosLosEventos
+{
+	public class NuevoSatelite : Evento
+	{
+		public NuevoSatelite()
+		{
+			Nombre = "New satelite";
+			FechaInicio = GestorTiempo.fechaActual;
+		}
+		public override void Inicio()
+		{
+
+			/* TODO
+			 * Mostrar una interfaz con el siguiente texto:
+			 * A new GPS probe sent by ESA has arrived and it will be added to your network.
+			 */
+
+			solucionado = true;
+			FechaFin = FechaInicio + new Fecha (0, 10, 0, 0);
+		}
+
+		public override void Tick()
+		{
+		}
+		public override void Fin()
+		{
+			if (solucionado) 
+			{
+				/* TODO
+				* Mostrar en la interfaz el siguiente texto:
+				* The new ESA satelite is online over the coordinates (las que sean).
+				*/
+
+				/* TODO
+				* añadir un satélite sobre esas coordenadas (probablemente elegidas de manera random entre partes del mapa no cubiertas.
+				*/
+			} 
+		}
+	}
+}
+
