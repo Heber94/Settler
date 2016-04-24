9ee6e75c1685d40a4debbd1e11d6867976b23c73
MetalBlueberry <victorperezdomingo@gmail.com>
2016-04-24 09:15:12 +0100

    Arreglado Lightning

---
+++ Assets/Scripts/GUIManagerScript.cs
@@ -0,0 +1,184 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+public partial class RefrescarInterfaz : MonoBehaviour
+{
+
+    /**
+	 * Cameras
+	 */
+    [Header("Cameras")]
+    //Camera in isometric
+    //private GameObject cameraVisualObject;
+    //Camera from above
+    private GameObject cameraPlayableObject;
+    //private Camera cameraVisual;
+    //private Camera cameraPlayable;
+    public GameObject cameraObject;
+   // private Camera camera;
+    private bool cameraPlayable;
+    public float speedCamera;
+	public float speedZoom;
+
+
+	[Header("Camera Limits")]
+	int MAX_X = 150;
+	int MIN_X = -110;
+	int MAX_Z = 90;
+	int MIN_Z = -90;
+
+	/**
+	 * Textos que se mostrarán por pantalla
+	 */
+
+	//public Text[] display;
+	/*[Header("Texts")]
+	public Text displayEnergy;
+	public Text displayScience;
+	public Text displayWater;
+	public Text displayFood;
+	public Text displayOxygen;
+	public Text displayPopulation;
+	public Text displayMaterials;*/
+
+	/**
+	 * Resources
+	 * Energy, Food, Oxygen, Science, Water, Population, Materials
+	 */
+
+	/**
+	 * Called in the begining
+	 * Search for the cameras components on cameras objects
+	 */
+
+	void Awake(){
+        //cameraVisual = cameraVisualObject.GetComponent<Camera> ();
+        //cameraPlayable = cameraPlayableObject.GetComponent<Camera> ();
+        //camera = cameraObject.GetComponent<Camera>();
+        cameraPlayableObject = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+	/**
+	 * Called in the begining after the Awake function
+	 * Initialize all the variables 
+	 */
+
+	/**
+	 * Called once per frame
+	 * Every frame you can change between cameras or control one of them
+	 * Also the text on the GUI is updated every frame
+	 */
+
+
+	/**
+	 * Change the text that is displayed on the GUI depending on the values of the resources
+	 */
+
+	public void ChangeResourcesText(){
+		//displayEnergy.text = ResourceManager.getEnergy().getName() + " : " + ResourceManager.getEnergy().getAmount();
+		/*displayScience.text = resources.getScience().getName() + " : " + resources.getScience().getAmount();
+		displayWater.text = resources.getWater().getName() + " : " + resources.getWater().getAmount();
+		displayFood.text = resources.getFood().getName() + " : " + resources.getFood().getAmount();
+		displayOxygen.text = resources.getOxygen().getName() + " : " + resources.getOxygen().getAmount();
+		displayPopulation.text = resources.getPopulation().getName() + " : " + resources.getPopulation().getAmount();
+		displayMaterials.text = resources.getMaterials().getName() + " : " + resources.getMaterials().getAmount();*/
+	}
+
+	/**
+	 * Hide the interface and all the values
+	 */
+
+	void HideText(){
+		/*displayEnergy.enabled = false;
+		displayScience.enabled = false;
+		displayWater.enabled = false;
+		displayFood.enabled = false;
+		displayOxygen.enabled = false;
+		displayPopulation.enabled = false;
+		displayMaterials.enabled = false;*/
+	}
+
+	/**
+	 * Display the interface and all the values
+	 */
+
+	void DisplayText(){
+		/*displayEnergy.enabled = true;
+		displayScience.enabled = true;
+		displayWater.enabled = true;
+		displayFood.enabled = true;
+		displayOxygen.enabled = true;
+		displayPopulation.enabled = true;
+		displayMaterials.enabled = true;*/
+	}
+
+	/**
+	 * Allows the user to moves the cameras depending on what is active
+	 */
+
+	public void ControlCameras(){
+			//Camera to the right
+			if ((Input.GetKey(KeyCode.D) && !cameraPlayable) || (Input.mousePosition.y <= (Screen.height * 0.05) && cameraPlayable)) {
+				cameraObject.transform.position = new Vector3 (
+					cameraObject.transform.position.x, 
+					cameraObject.transform.position.y, 
+					cameraObject.transform.position.z - (1 * speedCamera));
+			}
+			//Camera to the left
+			if ((Input.GetKey(KeyCode.A) && !cameraPlayable)|| (Input.mousePosition.y >= (Screen.height * 0.95) && cameraPlayable)) {
+				cameraObject.transform.position = new Vector3 (
+					cameraObject.transform.position.x, 
+					cameraObject.transform.position.y, 
+					cameraObject.transform.position.z + (1 * speedCamera));
+			}
+			//Camera to the top
+			if ((Input.GetKey (KeyCode.W) && !cameraPlayable) || (Input.mousePosition.x >= (Screen.width * 0.95) && cameraPlayable))
+        {
+            cameraObject.transform.position = new Vector3 (
+					cameraObject.transform.position.x + (1 * speedCamera), 
+					cameraObject.transform.position.y, 
+					cameraObject.transform.position.z);
+			}
+			//Camera to the bottom
+			if ((Input.GetKey (KeyCode.S) && !cameraPlayable) || (Input.mousePosition.x <= (Screen.width * 0.05) && cameraPlayable)) {
+				cameraObject.transform.position = new Vector3 (
+					cameraObject.transform.position.x - (1 * speedCamera), 
+					cameraObject.transform.position.y, 
+					cameraObject.transform.position.z);
+			}
+		if(cameraPlayable) {
+			//Zoom in
+			if(Input.GetAxis("Mouse ScrollWheel") > 0 && cameraPlayableObject.transform.position.y <= 100){
+				cameraPlayableObject.transform.position = new Vector3(
+					cameraPlayableObject.transform.position.x, 
+					cameraPlayableObject.transform.position.y + (1 * speedCamera * speedZoom), 
+					cameraPlayableObject.transform.position.z);
+			}
+			//Zoom out
+			if(Input.GetAxis("Mouse ScrollWheel") < 0 && cameraPlayableObject.transform.position.y >= 10){
+				cameraPlayableObject.transform.position = new Vector3(
+					cameraPlayableObject.transform.position.x, 
+					cameraPlayableObject.transform.position.y - (1 * speedCamera * speedZoom), 
+					cameraPlayableObject.transform.position.z);
+			}
+		}
+	}
+
+	/**
+	 * Change between cameras by pressing space
+	 */
+
+	public void ChangeCameras(){
+        //print("Cambiando");
+        //if (Input.GetKeyDown(KeyCode.Space)) {
+        //cameraVisual.enabled = !cameraVisual.enabled;
+        //cameraPlayable.enabled = !cameraPlayable.enabled;
+        GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().enabled = !GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().enabled;
+        GameObject.FindGameObjectWithTag("Camera").GetComponent<Camera>().enabled = !GameObject.FindGameObjectWithTag("Camera").GetComponent<Camera>().enabled;
+        cameraPlayable = !cameraPlayable;
+        //}
+    }
+		
+}
--- Assets/Scripts/Lightning.cs
+++ Assets/Scripts/Lightning.cs
@@ -7,43 +7,13 @@ public class Lightning : MonoBehaviour
    // public GameObject sun;
    // public GameObject deimos;
     ulong lastSeconds = 0;
-    float tiempoDia = 24 * 60 * 60;
+    ulong tiempoDia = 24 * 60 * 60;
     void Update()
     {
-        ulong secsActual = GestorTiempo.FechaActual.ToSeconds();
-        transform.RotateAround(Vector3.zero, Vector3.left,((float)(secsActual-lastSeconds)/tiempoDia)*360);
+        ulong secsActual = GestorTiempo.FechaActual.ToSeconds() % tiempoDia;
+        transform.rotation = Quaternion.AngleAxis(((float)(secsActual) /(float) tiempoDia) * 360, Vector3.left);//Vector3.zero, Vector3.left,((float)(secsActual-lastSeconds)/tiempoDia)*360);
+        transform.rotation *= Quaternion.AngleAxis(180, Vector3.left);
         lastSeconds = secsActual;
-      //  transform.RotateAround(Vector3.zero, Vector3.left, (float)rotation);
 
-        /*
-        ulong secsActuales = GestorTiempo.FechaActual.ToSeconds();
-        if (lastSeconds > secsActuales)
-        {
-            lastSeconds = 0;
-        }
-        ulong secsTranscurridos = secsActuales - lastSeconds;
-        print(GestorTiempo.FechaActual.ToString());
-
-        if (secsActuales > lastSeconds)
-        {
-            print("Secs actuales: " + secsActuales);
-            print("Last seconds: " + lastSeconds);
-            print("Transcurridos: " + secsTranscurridos);
-            //(260/(24*60*60)) = 0.004166666667
-            double rotation = secsTranscurridos * 0.004166666667;
-            print("Rotation: " + rotation.ToString());
-            lastSeconds = secsActuales;
-            transform.RotateAround(Vector3.zero, Vector3.left, (float)rotation);
-
-        }*/
-
-
-        //	void Update () {
-        //		float rotation = (GestorTiempo.FechaActual.ToSeconds()%(24*60*60))/(24*60*60);
-        //		//	print (rotation);
-        //		print (GestorTiempo.FechaActual.ToString ());
-        //		//transform.RotateAround (Vector3.zero, Vector3.left, rotation); 
-        //		transform.rotation = Quaternion.AngleAxis (rotation *2*Mathf.PI , Vector3.left);
-        //	}
     }
 }
